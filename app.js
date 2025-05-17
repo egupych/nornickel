4a798a50e97fab0d71f46322f29f443c62163b05
Dmitry Egupov <egupovds@gmail.com>
2025-05-17 08:58:13 +0500

    start

---
+++ app.js
@@ -0,0 +1,125 @@
+//::::::::::::::::::::::::::::::::::::::::::::::::::
+//Animation scroll to anchor
+//::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function scroll_part1(options = { block: "start", inline: "nearest", behavior: "smooth" }) {
+    const part1 = document.querySelector('.part1');
+    part1.scrollIntoView(options);
+}
+
+function scroll_part2(options = { block: "start", inline: "nearest", behavior: "smooth" }) {
+    const part2 = document.querySelector('.part2');
+    part2.scrollIntoView(options);
+}
+
+function scroll_part3(options = { block: "start", inline: "nearest", behavior: "smooth" }) {
+    const part3 = document.querySelector('.part3');
+    part3.scrollIntoView(options);
+}
+
+function scroll_part4(options = { block: "start", inline: "nearest", behavior: "smooth" }) {
+    const part4 = document.querySelector('.part4');
+    part4.scrollIntoView(options);
+}
+
+
+
+document.querySelectorAll('.option').forEach(option => {
+	option.addEventListener('click', () => {
+		const feedbackText = document.getElementById('feedback-text');
+		const answer = option.getAttribute('data-answer');
+
+		// Сброс стилей кнопок
+		document.querySelectorAll('.option').forEach(btn => {
+			btn.classList.remove('correct', 'wrong');
+		});
+
+		// Добавление стиля для выбранной кнопки
+		if (answer === 'correct') {
+			option.classList.add('correct');
+			feedbackText.textContent = 'Верно! Золото используется как проводник — им покрывают контакты. Старые гаджеты в этом смысле были богаче, но золото есть и в современных смартфонах. А также в организме человека — так что несколько миллиграммов золота, серебра и рутения есть и у вас.';
+		} else {
+			option.classList.add('wrong');
+			feedbackText.textContent = 'Неправильно, попробуйте еще раз';
+		}
+	});
+});
+
+document.querySelectorAll('.option2').forEach(option2 => {
+	option2.addEventListener('click', () => {
+		const feedbackText2 = document.getElementById('feedback-text2');
+		const answer2 = option2.getAttribute('data-answer2');
+
+		// Сброс стилей кнопок
+		document.querySelectorAll('.option2').forEach(btn => {
+			btn.classList.remove('correct2', 'wrong2');
+		});
+
+		// Добавление стиля для выбранной кнопки
+		if (answer2 === 'correct2') {
+			option2.classList.add('correct2');
+			feedbackText2.textContent = 'По одной из самых популярных теорий, драгоценные металлы попали на Землю в результате столкновений с космическими телами. Этим объясняется тот факт, что металлы распределены по земной поверхности неравномерно. ';
+		} else {
+			option2.classList.add('wrong2');
+			feedbackText2.textContent = 'Неправильно, попробуйте еще раз';
+		}
+	});
+});
+
+document.querySelectorAll('.option3').forEach(option3 => {
+	option3.addEventListener('click', () => {
+		const feedbackText3 = document.getElementById('feedback-text3');
+		const answer3 = option3.getAttribute('data-answer3');
+
+		// Сброс стилей кнопок
+		document.querySelectorAll('.option3').forEach(btn => {
+			btn.classList.remove('correct3', 'wrong3');
+		});
+
+		// Добавление стиля для выбранной кнопки
+		if (answer3 === 'correct3') {
+			option3.classList.add('correct3');
+			feedbackText3.textContent = 'Самым крупным самородком, найденным в Красноярском крае, по праву считается «Бычья голова» весом 31 кг и 570 гр. Его подняли 10 января 1898 года на Спасо-Преображенском прииске. До наших дней он не сохранился, но существует точный муляж. Посмотреть его можно в Музее драгоценных металлов Красцветмета. ';
+		} else {
+			option3.classList.add('wrong3');
+			feedbackText3.textContent = 'Неправильно, попробуйте еще раз';
+		}
+	});
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	const contents = document.querySelectorAll('.content'); // Находим все блоки
+  
+	if (!contents.length) return; // Если нет ни одного блока, выходим
+  
+	const observer = new IntersectionObserver((entries, observer) => {
+	  entries.forEach(entry => {
+		if (entry.isIntersecting) {
+		  entry.target.classList.add('animate'); // Добавляем класс анимации
+		  observer.unobserve(entry.target); // Останавливаем наблюдение для этого блока
+		}
+	  });
+	}, { threshold: 0.3 });
+  
+	contents.forEach(content => observer.observe(content)); // Наблюдаем за каждым блоком
+  });
+  
+
+  // Функция для отслеживания, когда карточка попадает в область видимости
+const observer = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+	  if (entry.isIntersecting) {
+		entry.target.classList.add('visible'); // Добавляем класс для анимации
+		observer.unobserve(entry.target); // Останавливаем отслеживание, когда карточка появилась
+	  }
+	});
+  }, { threshold: 0.5 }); // 50% карточки должно быть видно
+  
+  // Наблюдаем за всеми карточками
+  const cards = document.querySelectorAll('.card');
+  cards.forEach(card => {
+	observer.observe(card);
+  });
+  
+
+  
